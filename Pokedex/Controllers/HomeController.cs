a98d6dc0860465fe385d84fc57038b5998dbd301
Luiz <lui-otavio@hotmail.com>
2024-03-13 14:43:43 -0300

    Ajuda do Galo

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -17,16 +17,43 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         List<Pokemon>pokemons = [];
-        using (StreamReader leitor = new("Data\\pokenos.json")){
+        using (StreamReader leitor = new("Data\\pokemons.json")){
             string dados = leitor.ReadToEnd();
             pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
         }
+        List<Tipo> tipos = [];
+         using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+         ViewData["Tipos"] = tipos;
+
         return View(pokemons);
     }
+
      public IActionResult Details(int id)
     {
-        return View();
+        List<Pokemon>pokemons = [];
+        using (StreamReader leitor = new("Data\\pokemons.json")){
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo>tipos = [];
+        using ( StreamReader leitor = new("Data\\tipos.json")){
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        
+        DetailsVM details = new(){
+            Tipos = tipos,
+            Atual = pokemons.FirstOrDefault(p => p.Numero == id),
+            Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
+            Proximo = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),
+        };
+        return View(details);
     }
+
     public IActionResult Privacy()
     {
         return View();
